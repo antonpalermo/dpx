52cf993b1db7250744dce19e8555118c3b2aced2
Anton Palermo <anton.palermo+github@dragonpay.ph>
2024-10-25 22:44:58 +0800

    wip: setup tsup bundler

--- packages/collect/src/index.ts
+++ packages/collect/src/index.ts
@@ -12,7 +12,7 @@ export interface CollectionClientOptions {
   secret: string;
 
   /**
-   *
+   * API optional parameters
    */
   options?: {
     /**
@@ -36,7 +36,6 @@ export default function CollectionClient({
 
   /**
    * wraps the native fetch.
-   *
    * @param endpoint endpoint to send the request to.
    * @param options request options
    * @returns fetch response.
@@ -56,35 +55,53 @@ export default function CollectionClient({
 
   /**
    * create a new collection transaction.
-   *
    * @param txnid unique transaction id that represent the whole transaction.
    * @param data data that dragonpay will process
    */
   async function collect(txnid: string, data: {}) {
-    const req = await request(`${endpoint}/${txnid}/post`, {
-      method: "POST",
-      body: JSON.stringify(data)
-    });
+    try {
+      const req = await request(`${endpoint}/${txnid}/post`, {
+        method: "POST",
+        body: JSON.stringify(data)
+      });
 
-    console.log(await req.json());
+      return await req.json();
+    } catch (error) {
+      console.log("collect: unable to process request");
+      throw error;
+    }
   }
 
   /**
    * get transaction details using the provided refno
    * @param refno unique reference number assigned to a transaction.
    */
-  async function getRefnoDetails(refno: string) {
-    const req = await request(`${endpoint}/refno/${refno}`, { method: "GET" });
-    console.log(await req.json());
+  async function getTransactionByRefno(refno: string) {
+    try {
+      const req = await request(`${endpoint}/refno/${refno}`, {
+        method: "GET"
+      });
+      return await req.json();
+    } catch (error) {
+      console.log("getTransactionByRefno: unable to process request");
+      throw error;
+    }
   }
 
   /**
    * get transaction details using the provided txnid
    * @param txnid unique transaction id assigned to a transaction.
    */
-  async function getTxnidDetails(txnid: string) {
-    const req = await request(`${endpoint}/txnid/${txnid}`, { method: "GET" });
-    console.log(await req.json());
+  async function getTransactionByTxnid(txnid: string) {
+    try {
+      const req = await request(`${endpoint}/txnid/${txnid}`, {
+        method: "GET"
+      });
+      return await req.json();
+    } catch (error) {
+      console.log("getTransactionByTxnid: unable to process request");
+      throw error;
+    }
   }
 
   /**
@@ -92,9 +109,19 @@ export default function CollectionClient({
    * @param txnid unique transaction id assigned to a transaction.
    */
   async function cancelTransaction(txnid: string) {
-    const req = await request(`${endpoint}/void/${txnid}`, { method: "GET" });
-    console.log(await req.json());
+    try {
+      const req = await request(`${endpoint}/void/${txnid}`, { method: "GET" });
+      return await req.json();
+    } catch (error) {
+      console.log("cancelTransaction: unable to process request");
+      throw error;
+    }
   }
 
-  return { collect, getRefnoDetails, getTxnidDetails, cancelTransaction };
+  return {
+    collect,
+    cancelTransaction,
+    getTransactionByRefno,
+    getTransactionByTxnid
+  };
 }
