22425bda818a0f7bbadaf81c088d102c683fa1b0
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-10-29 21:09:10 +0800

    refac: removed old packages

--- packages/collections/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "@/utils";
-export * from "@/libs/client";
-export * from "@/libs/lifetime";
-export * from "@/libs/collections";
--- packages/collections/src/types/lid-details.d.ts
+++
@@ -1,5 +0,0 @@
-export type LifetimeDetails = {
-  name: string;
-  email: string;
-  remarks: string;
-};
