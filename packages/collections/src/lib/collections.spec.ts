7b3d219360e9a7e2602cc07e5a179f0b5ba4ac57
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-04-19 23:07:15 +0800

    moved collection functions

--- packages/collections/src/lib/collections.spec.ts
+++ packages/collections/src/lib/collections.spec.ts
@@ -1,7 +1,16 @@
-import { collections } from './collections.js';
+import { CollectionClient } from './collections.js';
 
-describe('collections', () => {
-  it('should work', () => {
-    expect(collections()).toEqual('collections');
+describe('collections unit testing', () => {
+  const client = CollectionClient({ mid: '', secretKey: '' });
+
+  it('can create collection transaction', async () => {
+    await client.collect('', {
+      amount: 1.0,
+      currency: 'PHP',
+      email: '',
+      description: 'Test Payment',
+    });
+
+    expect('test').toBe('test');
   });
 });
--- packages/collections/src/lib/collections.ts
+++ packages/collections/src/lib/collections.ts
@@ -1,3 +1,191 @@
-export function collections(): string {
-  return 'collections';
+import { Buffer } from 'node:buffer';
+
+export type Option = {
+  mid: string;
+  secretKey: string;
+  api?: {
+    version: 'v1' | 'v2';
+  };
+};
+
+export interface BillingInfo {
+  firstname: string;
+  lastname: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  country: string;
+  zipCode: string;
+  telno: string;
+  email: string;
+}
+
+export interface ShippingInfo {
+  firstname: string;
+  middlename: string;
+  lastname: string;
+  address1: string;
+  address2: string;
+  barangay: string;
+  city: string;
+  province: string;
+  country: string;
+  zipCode: string;
+  landmark: string;
+  telno: string;
+  email: string;
+}
+
+export interface Transaction {
+  amount: number;
+  currency: 'PHP';
+  description: string;
+  email: string;
+  mobileNo?: string;
+  param1?: string;
+  param2?: string;
+  expiry?: Date;
+}
+
+export function CollectionClient({
+  mid,
+  secretKey,
+  api = { version: 'v2' },
+}: Option) {
+  const env = process.env.NODE_ENV === 'development' ? 'test' : 'gw';
+
+  const endpoint = `https://${env}.dragonpay.ph/api/collect/${api.version}`;
+
+  const Authorization = `Basic ${Buffer.from(`${mid}:${secretKey}`).toString(
+    'base64',
+  )}`;
+
+  async function collect(txnid: string, data: Transaction) {
+    try {
+      const req = await fetch(`${endpoint}/${txnid}/post`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization,
+        },
+        body: JSON.stringify(data),
+      });
+
+      return await req.json();
+    } catch (error) {
+      console.log(error);
+      return { error: 'An error occurred while collecting data.' };
+    }
+  }
+
+  /**
+   * get transaction details using the provided refno
+   * @param refno unique reference number assigned to a transaction.
+   */
+  async function getTransactionByRefno(refno: string) {
+    try {
+      const request = await fetch(`${endpoint}/refno/${refno}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization,
+        },
+      });
+
+      return await request.json();
+    } catch (error) {
+      console.log('getTransactionByRefno: unable to process request');
+      throw error;
+    }
+  }
+
+  /**
+   * get transaction details using the provided txnid
+   * @param txnid unique transaction id assigned to a transaction.
+   */
+  async function getTransactionByTxnid(txnid: string) {
+    try {
+      const request = await fetch(`${endpoint}/txnid/${txnid}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization,
+        },
+      });
+
+      return await request.json();
+    } catch (error) {
+      console.log('getTransactionByTxnid: unable to process request');
+      throw error;
+    }
+  }
+
+  /**
+   * void transaction based on the provided txnid.
+   * @param txnid unique transaction id assigned to a transaction.
+   */
+  async function cancelTransaction(txnid: string) {
+    try {
+      const request = await fetch(`${endpoint}/void/${txnid}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization,
+        },
+      });
+      return await request.json();
+    } catch (error) {
+      console.log('cancelTransaction: unable to process request');
+      throw error;
+    }
+  }
+
+  async function getTransactions(from: Date, to: Date) {
+    const parseDate = (date: Date) =>
+      `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+
+    try {
+      const request = await fetch(
+        `${endpoint}/transactions?startdate=${parseDate(from)}&enddate=${parseDate(to)}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization,
+          },
+        },
+      );
+      return await request.json();
+    } catch (error) {
+      console.log('cancelTransaction: unable to process request');
+      throw error;
+    }
+  }
+
+  async function getSettledTransactions(from: Date, to: Date) {
+    const parseDate = (date: Date) =>
+      `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+
+    try {
+      const request = await fetch(
+        `${endpoint}/settled?startdate=${parseDate(from)}&enddate=${parseDate(to)}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization,
+          },
+        },
+      );
+      return await request.json();
+    } catch (error) {
+      console.log('cancelTransaction: unable to process request');
+      throw error;
+    }
+  }
+
+  return {
+    collect,
+    getTransactions,
+    cancelTransaction,
+    getTransactionByRefno,
+    getTransactionByTxnid,
+    getSettledTransactions,
+  };
 }
